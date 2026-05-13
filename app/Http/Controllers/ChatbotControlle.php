5f03bd8fa9cd13afcb1946b01241d399312b0e09
Nawaf Akbar <nwfakbart@gmail.com>
2026-05-13 12:42:26 +0700

    chatbot2

--- app/Http/Controllers/ChatbotControlle.php
+++ app/Http/Controllers/ChatbotControlle.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
-class ChatbotController extends Controller
+class ChatbotControlle extends Controller
 {
     // =========================================================
     // KONFIGURASI MODEL
